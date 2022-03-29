98f2210675c24a3473cfa5ee91d6004c9e4c24b0
Jaedotpy <101655762+Jaedotpy@users.noreply.github.com>
2022-03-29 07:12:56 -0500

    De la mano del señor

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,70 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+	
+	// - Getters and Setters -
+	
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+
+	// - Methods - 
+	
+	public void turnOn(){
+		tv.estado = true;
+	}
+	
+	public void turnOff() {
+		tv.estado = false;
+	}
+	
+	public void canalUp(){
+		if (tv.estado = true) {
+			if(tv.canal != 120 && tv.canal >= 1) {
+				tv.canal  = tv.canal + 1;
+			}			
+		}		
+	}
+	
+	public void canalDown(){
+		if (tv.estado = true) {
+			if(tv.canal != 1 && tv.canal <= 120) {
+				tv.canal  = tv.canal - 1;
+			}			
+		}		
+	}
+	
+	public void volumenUp() {
+		if (tv.estado = true) {
+			if(tv.volumen != 7 && tv.volumen >= 1) {
+				tv.volumen  = tv.volumen + 1;
+			}			
+		}		
+	}
+	
+	public void volumenDown() {
+		if (tv.estado = true) {
+			if(tv.volumen != 1 && tv.volumen <= 7) {
+				tv.volumen  = tv.volumen - 1;
+			}			
+		}		
+	}
+	
+	public void setCanal(int canal) {
+		if (tv.estado = true) {
+			if (canal <= 120 && canal >= 1) {
+				tv.canal = canal;
+			}
+		}
+	}
+	
+	public void enlazar(TV tvEnlazado) {
+		this.tv = tvEnlazado;
+	}
+	
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,113 @@
+package taller3.televisores;
+public class TV {
+	private Marca marca;
+	public int canal;
+	private int precio;
+	public boolean estado;
+	public int volumen;
+	private Control control;
+	private int numTV = 0;
+	
+	// - Constructor -
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.canal = 1;
+		this.precio = 500;
+		this.estado = estado;
+		this.volumen = 1;
+		numTV = numTV + 1;
+	}
+	
+	// - Getters and Setters -
+	
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public int getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+
+	public Control getControl() {
+		return control;
+	}
+
+	public void setControl(Control control) {
+		this.control = control;
+	}
+	
+	public int getNumTV() {
+		return numTV;
+	}
+	
+	public void turnOn(){
+		this.estado = true;
+	}
+	
+	public void turnOff() {
+		this.estado = false;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	// - Methods - 
+	
+	public void canalUp(){
+		if (this.estado = true) {
+			if(this.canal != 120 && this.canal >= 1) {
+				this.canal  = this.canal + 1;
+			}			
+		}		
+	}
+	
+	public void canalDown(){
+		if (this.estado = true) {
+			if(this.canal != 1 && this.canal <= 120) {
+				this.canal  = this.canal - 1;
+			}			
+		}		
+	}
+	
+	public void volumenUp() {
+		if (this.estado = true) {
+			if(this.volumen != 7 && this.volumen >= 1) {
+				this.volumen  = this.volumen + 1;
+			}			
+		}		
+	}
+	
+	public void volumenDown() {
+		if (this.estado = true) {
+			if(this.volumen != 1 && this.volumen <= 7) {
+				this.volumen  = this.volumen - 1;
+			}			
+		}		
+	}	
+}
